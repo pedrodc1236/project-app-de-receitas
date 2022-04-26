a0fab514a5d2db4bd47d04aa2d52d90857d55396
Pedro Tavares <pedrodc1236@gmail.com>
2022-04-26 18:33:11 -0300

    tela de login

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Login from './Pages/Login';
+import Foods from './Pages/Foods';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/foods" component={ Foods } />
       </Switch>
     </BrowserRouter>
   );
