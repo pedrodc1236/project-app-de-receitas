cf3282dfa336e84ab94af58fe4abc0469fec14c9
Lucas Nascimento <lucasnascimento.lp@gmail.com>
2022-04-26 23:53:36 -0300

    feat: Criação do Header
    Requisitos 09, 11 e 12 OK

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Login from './Pages/Login';
+import Profile from './Pages/Profile';
+import Foods from './Pages/Foods';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/foods" component={ Foods } />
       </Switch>
     </BrowserRouter>
   );
