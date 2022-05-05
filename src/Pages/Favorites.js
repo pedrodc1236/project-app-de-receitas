def95de7fcb6deb44dc096f581e31fd015a06882
Vítor Pacheco <vitor050401@gmail.com>
2022-05-05 17:51:37 -0300

    requisito 63 saindo do forno ala ele la

--- src/Pages/Favorites.js
+++ src/Pages/Favorites.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
 import ShareIcon from '../images/shareIcon.svg';
+import Snackbar from '../Components/Snackbar';
 import BlackHeartIcon from '../images/blackHeartIcon.svg';
 // import WhiteHeartIcon from '../images/whiteHeartIcon.svg';
 import '../services/Favorites.css';
 
+const THREE_SECONDS = 3000;
+
 function Favorites() {
+  const [showSnackbar, setShowSnackbar] = useState(false);
+
   const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
   console.log(favoriteRecipes);
 
@@ -42,41 +47,59 @@ function Favorites() {
           category,
           nationality,
           alcoholicOrNot } = favRecipe;
+
+        const handleShareButton = () => {
+          navigator.clipboard.writeText(`http://localhost:3000/foods/${id}`);
+          setShowSnackbar(true);
+          setTimeout(() => {
+            setShowSnackbar(false);
+          }, THREE_SECONDS);
+        };
         return (
-          <Link to={ `/${type}s/${id}` } key={ index }>
-            <div>
-              <img
-                data-testid={ `${index}-horizontal-image` }
-                src={ image }
-                alt={ name }
-                className="fav-recipe-img"
+          <div key={ index }>
+            <Link to={ `/${type}s/${id}` }>
+              <div>
+                <img
+                  data-testid={ `${index}-horizontal-image` }
+                  src={ image }
+                  alt={ name }
+                  className="fav-recipe-img"
+                />
+                <div className="fav-recipe-info">
+                  <h3 data-testid={ `${index}-horizontal-name` }>{name}</h3>
+                  <p
+                    data-testid={ `${index}-horizontal-top-text` }
+                  >
+                    { type === 'food'
+                      ? `${nationality} - ${category}`
+                      : `${alcoholicOrNot}`}
+                  </p>
+                </div>
+              </div>
+            </Link>
+            <div className="fav-page-icons">
+              <input
+                type="image"
+                src={ ShareIcon }
+                alt="Share Icon"
+                data-testid={ `${index}-horizontal-share-btn` }
+                onClick={ handleShareButton }
               />
-              <div className="fav-recipe-info">
-                <h3 data-testid={ `${index}-horizontal-name` }>{name}</h3>
-                <p
-                  data-testid={ `${index}-horizontal-top-text` }
-                >
-                  { type === 'food'
-                    ? `${nationality} - ${category}`
-                    : `${alcoholicOrNot}`}
-                </p>
-              </div>
-              <div className="fav-page-icons">
-                <input
-                  type="image"
-                  src={ ShareIcon }
-                  alt="Share Icon"
-                  data-testid={ `${index}-horizontal-share-btn` }
-                />
-                <input
-                  type="image"
-                  src={ BlackHeartIcon }
-                  alt="Black Heart Icon"
-                  data-testid={ `${index}-horizontal-favorite-btn` }
-                />
-              </div>
+              <Snackbar
+                open={ showSnackbar }
+                onClose={ () => setShowSnackbar(false) }
+              >
+                Link copied!
+              </Snackbar>
+              <input
+                type="image"
+                src={ BlackHeartIcon }
+                alt="Black Heart Icon"
+                data-testid={ `${index}-horizontal-favorite-btn` }
+              />
             </div>
-          </Link>
+          </div>
+
         );
       }) }
     </>
